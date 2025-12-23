452be638dacb128a7cdd231c4ba7f4d3e469329e
mandi <mandi@et.esiea.fr>
2025-12-23 23:40:42 +0100

    Configure GitHub Pages

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,10 @@
+function Contact() {
+    return (
+      <section>
+        <h1>Contact</h1>
+        <p>Vous pouvez me contacter par email .</p>
+      </section>
+    )
+  }
+  
+  export default Contact
---
+++ src/pages/Projects.tsx
@@ -0,0 +1,10 @@
+function Projects() {
+    return (
+      <section>
+        <h1>Projets</h1>
+        <p>Présentation de mes projets.</p>
+      </section>
+    )
+  }
+  
+  export default Projects
