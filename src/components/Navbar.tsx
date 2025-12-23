e3ef6e7cb3912c3e9e0752aa47ba5bb1ca81ea3b
mandi <mandi@et.esiea.fr>
2025-12-23 23:30:34 +0100

    modif

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">Portfolio</div>
+      <ul className="nav-links">
+        <li><Link to="/">Accueil</Link></li>
+        <li><Link to="/about">À propos</Link></li>
+        <li><Link to="/projects">Projets</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
