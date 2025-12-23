5b12f35a89cf7ec9a468e78c45f7684949c5efc0
mandi <mandi@et.esiea.fr>
2025-12-23 23:25:17 +0100

    Initial portfolio

---
+++ src/pages/About.tsx
@@ -0,0 +1,15 @@
+function About() {
+    return (
+      <section>
+        <h1>À propos</h1>
+        <p>
+          Je suis étudiant en ingénierie informatique. Mon parcours m’a permis
+          de développer des compétences solides en programmation, en
+          développement web et en travail en équipe à travers des projets
+          académiques et personnels.
+        </p>
+      </section>
+    )
+  }
+  
+  export default About
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+function Home() {
+    return (
+      <section>
+        <h1>Kamil Mandi</h1>
+        <h2>Étudiant en ingénierie informatique</h2>
+        <p>
+          Passionné par le développement logiciel et les projets concrets,
+          je construis des applications modernes orientées web et data.
+        </p>
+      </section>
+    )
+  }
+  
+  export default Home
